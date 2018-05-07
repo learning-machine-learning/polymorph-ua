36d0d55eba7998789e2cd09082f63d9b59669d75
c0dyzeng <codyzeng@berkeley.edu>
2018-05-06 20:12:02 -0700

    added most recent aws scripts

---
+++ aws-scripts/combiner-hourly-pos-merged-logs.py
@@ -0,0 +1,82 @@
+import gzip
+import json
+
+import boto3
+import os
+
+s3 = boto3.resource('s3')
+s3client = boto3.client('s3')
+
+as_bucket_name = 'codebase-pm-ua-filtered'
+cb_bucket_name = 'codebase-pm-ua-filtered'
+
+TEMP_INPUT_FILE_NAME = 'temp_in.gz'
+TEMP_OUTPUT_FILE_NAME = 'temp_out.gz'
+
+# Total Files: 15,360
+# START_INDEX = 0
+# STOP_INDEX = 2000
+
+CONTINUATION_TOKEN = None
+
+try:
+    os.remove(TEMP_INPUT_FILE_NAME)
+except OSError:
+    pass
+try:
+    os.remove(TEMP_OUTPUT_FILE_NAME)
+except OSError:
+    pass
+
+as_objects = s3client.list_objects_v2(Bucket=as_bucket_name)
+
+as_bucket = s3.Bucket(name=as_bucket_name)
+cb_bucket = s3.Bucket(name=cb_bucket_name)
+
+# file_keys = [item['Key'] for item in as_objects['Contents'] if 'part' in item['Key']][START_INDEX:STOP_INDEX]
+
+file_keys = []
+combined_keys = []
+for h in range(	0, 24):
+    for n in range(0, 128):
+        file_keys.append('merged_logs/%02d/%02d/part-00%03d_positives.gz' % (5, h , n))
+    combined_keys.append('merged_logs/%02d/%02d/combined_positives.gz' % (5, h))
+
+# file_keys = file_keys[START_INDEX:STOP_INDEX]
+
+# keys_to_pop = [
+#     # Duplicate Keys
+#     "r_timestamp", "pub_network_id", "geo_country_code2", "site_id", "session_id", "geo_continent_code", "zone_id",
+#     # Unique Keys
+#     "bid_req_cnt", "bid_resp_cnt", "bid_price", "campaign_id"
+# ]
+
+zip_count = 0
+combined_key_count = 0
+for file_key in file_keys:
+    zip_count += 1
+    print(file_key)
+    try:
+        as_bucket.download_file(Key=file_key, Filename=TEMP_INPUT_FILE_NAME)
+        with gzip.open(TEMP_INPUT_FILE_NAME, 'rt') as f:
+            lines = f.readlines()
+            f.close()
+
+        if zip_count == 1:
+            filtered_output = gzip.open(TEMP_OUTPUT_FILE_NAME, 'wt')
+
+        index = 0
+        while index < len(lines):
+            line_dict = json.loads(lines[index])
+            line = json.dumps(line_dict)
+            filtered_output.write(line + "\n")
+            index += 1
+
+    except:
+        print("failed to add file of " + str(file_key) + " to combined gzip! Sad!")
+    if zip_count >= 127:
+        zip_count = 0
+        filtered_output.close()
+        cb_bucket.upload_file(Filename=TEMP_OUTPUT_FILE_NAME, Key=combined_keys[combined_key_count])
+        combined_key_count += 1
+
---
+++ aws-scripts/filterer-positives-only.py
@@ -0,0 +1,83 @@
+import gzip
+import json
+
+import boto3
+import os
+
+s3 = boto3.resource('s3')
+s3client = boto3.client('s3')
+
+as_bucket_name = 'adsnative-sigmoid'
+cb_bucket_name = 'codebase-pm-ua-filtered'
+
+TEMP_INPUT_FILE_NAME = 'temp_in.gz'
+TEMP_OUTPUT_FILE_NAME = 'temp_out.gz'
+
+# Total Files: 15,360
+START_INDEX = 0
+STOP_INDEX = 2000
+
+CONTINUATION_TOKEN = None
+
+try:
+    os.remove(TEMP_INPUT_FILE_NAME)
+except OSError:
+    pass
+try:
+    os.remove(TEMP_OUTPUT_FILE_NAME)
+except OSError:
+    pass
+
+as_objects = s3client.list_objects_v2(Bucket=as_bucket_name)
+
+as_bucket = s3.Bucket(name=as_bucket_name)
+cb_bucket = s3.Bucket(name=cb_bucket_name)
+
+# file_keys = [item['Key'] for item in as_objects['Contents'] if 'part' in item['Key']][START_INDEX:STOP_INDEX]
+
+file_keys = []
+for d in range(5, 8):
+    for h in range(0, 24):
+        for n in range(0, 128):
+            file_keys.append('merged_logs/%02d/%02d/part-00%03d.gz' % (d, h, n))
+
+file_keys = file_keys[START_INDEX:STOP_INDEX]
+
+# keys_to_pop = [
+#     # Duplicate Keys
+#     "r_timestamp", "pub_network_id", "geo_country_code2", "site_id", "session_id", "geo_continent_code", "zone_id",
+#     # Unique Keys
+#     "bid_req_cnt", "bid_resp_cnt", "bid_price", "campaign_id"
+# ]
+
+for file_key in file_keys:
+    print(file_key)
+    try:
+        as_bucket.download_file(Key=file_key, Filename=TEMP_INPUT_FILE_NAME)
+
+        with gzip.open(TEMP_INPUT_FILE_NAME, 'rt') as f:
+            lines = f.readlines()
+            f.close()
+        filtered_output = gzip.open(TEMP_OUTPUT_FILE_NAME, 'wt')
+
+        index = 0
+        while index < len(lines):
+            line_dict = json.loads(lines[index])
+            try:
+                index += 1
+                if "c_cnt" in line_dict and line_dict["c_cnt"] > 0:
+                    # bid_line_dict = json.loads(lines[index])
+                    # for k in keys_to_pop:
+                    #     bid_line_dict.pop(k, None)
+                    # line_dict.update(bid_line_dict)
+                    # index += len(line_dict["bid_requests"])
+                    line = json.dumps(line_dict)
+                    filtered_output.write(line + "\n")
+            except:
+                continue
+
+        filtered_output.close()
+
+        cb_bucket.upload_file(Filename=TEMP_OUTPUT_FILE_NAME, Key=file_key[:-3] + "_positives.gz")
+    except:
+        print("filename of " + str(file_key) + " failed! Sad!")
